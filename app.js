9c9b34eec48b409ced3db61f03bff2d7148ed347
Ravi Sinhmar <108986605+Ravi-Sinhmar@users.noreply.github.com>
2022-08-05 10:27:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
+
+});
+app.post("/", function(req, res) {
+    var firstname = req.body.fname;
+    var lastname = req.body.lname;
+    var email = req.body.email;
+    res.write("<h1> Thanks... </h1>");
+    res.write("<h1> This response is not going anywhere so Chill</h1>");
+
+
+    res.write("<h3 > Your First Name is " + firstname + "</h3>");
+    res.write("<h3> Your Last Name is " + lastname + "</h3>");
+
+    res.write("<h3> Your Email is " + email + "</h3>");
+    res.send();
+
+
+})
+
+
+
+app.listen(3000, function() {
+
+});
