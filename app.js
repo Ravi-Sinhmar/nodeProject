f18728febfd6a278d5af8f3f80dae9119de3335c
Ravi Sinhmar <108986605+Ravi-Sinhmar@users.noreply.github.com>
2022-08-05 10:30:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/signup.html");
+    res.sendFile(__dirname + "/index.html");
 
 });
 app.post("/", function(req, res) {
